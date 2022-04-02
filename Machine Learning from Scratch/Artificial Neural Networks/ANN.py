6c6d163431f56c1b17f85d5af1430df1a7ffb251
isalia99 <irakli.salia854@gmail.com>
2022-04-02 21:47:03 +0400

    renamed folder

--- Machine Learning from Scratch/Artificial Neural Networks/ANN.py
+++ Machine Learning from Scratch/Artificial Neural Networks/ANN.py
@@ -2,4 +2,18 @@
 
 import numpy as np
 
-class FF_Neural_Network:
+class NeuralNetwork:
+
+    def __init__(self,
+                 n_layers = 1,
+                 activation = "relu",
+                 solver = "adam",
+                 alpha = 0.01,
+                 random_state = 42):
+        self.n_layers = n_layers
+        self.activation = activation
+        self.solver = solver
+        self.alpha = alpha
+        self.random_state = random_state
+
+    def 
