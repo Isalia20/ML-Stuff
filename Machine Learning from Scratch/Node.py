8dda0a0a5b83090a0900438ebdb5195ec5f54d1e
isalia20 <irakli.salia854@gmail.com>
2022-04-02 17:45:53 +0400

    tmp change

--- Machine Learning from Scratch/Node.py
+++ Machine Learning from Scratch/Node.py
@@ -1,10 +1,13 @@
 class Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None, n_count = None,
+                 value_for_pruning=None):
         self.feature = feature
         self.threshold = threshold
         self.left = left
         self.right = right
         self.value = value
+        self.n_count = n_count
+        self.value_for_pruning = value_for_pruning
 
     def is_leaf(self):
         if self.value is None:
