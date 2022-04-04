acd0f43405eefb50e65e2e68fa0a7cb3b1021fbb
isalia99 <irakli.salia854@gmail.com>
2022-04-04 23:49:57 +0400

    added some methods

--- Machine Learning from Scratch/Artificial Neural Networks/ANN.py
+++ Machine Learning from Scratch/Artificial Neural Networks/ANN.py
@@ -1,16 +1,33 @@
-#WIP
+# WIP
 import numpy as np
 
-np.array([4,5,3])
 
-
-class NeuralNetwork:
+class FeedForwardNeuralNetwork:
 
     def __init__(self,
-                 input_shape=(200,5),
                  random_state=42):
-        self.input_shape = input_shape
+        self.random_state = random_state
+        self.network_architecture = {}
+        self.weight_matrices = {}
 
-    def _initialize_network(self):
-        m = self.input_shape[0]
-        n = self.input_shape[1]
+    def _initialize_network(self, x):
+        self.network_architecture[0] = x.shape[1]
+
+    def add_change_layer(self, layer_index, n_neurons):
+        if layer_index < 0:
+            raise Exception("You can't change/add layer before input")
+        elif layer_index == 0:
+            raise Exception("You can't change input layer's neuron count. Change shape of X and reinitialize class")
+        self.network_architecture[layer_index] = n_neurons
+
+    def _generate_weight_matrices(self):
+        for layer_index in range(1, self.network_architecture.keys()):
+            self.weight_matrices[layer_index] = \
+                self.network_architecture = \
+                np.random.normal(0, 1,
+                                 (self.network_architecture[layer_index - 1],
+                                  self.network_architecture[layer_index]))
+
+    # def _feed_forward(self, x):
+    #     x = np.matrix(x)
+    #     np.matmul(x, self.weight_matrices[0])
