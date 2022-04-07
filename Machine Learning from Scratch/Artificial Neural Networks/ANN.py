b623c9cc68c64f8f82815666ba11f0d85df31229
isalia99 <irakli.salia854@gmail.com>
2022-04-07 23:18:48 +0400

    added bias boolean parameter

--- Machine Learning from Scratch/Artificial Neural Networks/ANN.py
+++ Machine Learning from Scratch/Artificial Neural Networks/ANN.py
@@ -5,7 +5,8 @@ import numpy as np
 class FeedForwardNeuralNetwork:
 
     def __init__(self,
-                 random_state=42):
+                 random_state=42,
+                 ):
         self.random_state = random_state
         self.network_architecture = {}
         self.weight_matrices = {}
@@ -13,32 +14,37 @@ class FeedForwardNeuralNetwork:
     def _initialize_network(self, x):
         self.network_architecture[0] = x.shape[1]
 
-    def add_change_layer(self, layer_index, n_neurons, activation_function):
+    def add_change_layer(self, layer_index, n_neurons, activation_function, bias_neuron = True):
         if layer_index < 0:
             raise Exception("You can't change/add layer before input")
         elif layer_index == 0:
             raise Exception("You can't change input layer's neuron count. Change shape of X and reinitialize class")
-        self.network_architecture[layer_index] = (n_neurons,activation_function)
+        self.network_architecture[layer_index] = (n_neurons, activation_function, bias_neuron)
 
-    def _generate_weight_matrices(self):
+    def _generate_weight_matrices(self, activation_function):
         for layer_index in range(1, self.network_architecture.keys()):
             self.weight_matrices[layer_index] = \
-                self.network_architecture = \
-                np.random.normal(0, 1,
-                                 (self.network_architecture[layer_index - 1],
-                                  self.network_architecture[layer_index]))
+                np.random.normal(0, 1, (self.network_architecture[layer_index - 1],
+                                        self.network_architecture[layer_index]))
 
-    def _feed_forward(self, x, layer_index):
-        x = np.matrix(x)
+    def _feed_forward(self, last_layer_output, layer_index):
+        x = np.matrix(last_layer_output)
         weight_matrix = self.weight_matrices[layer_index]
+        if self.network_architecture[layer_index][2]:
+            pre_activation = np.matmul(x, weight_matrix)
+
         pre_activation = np.matmul(x, weight_matrix)
-        if weight_matrix[layer_index][1] == "relu":
+        if self.network_architecture[layer_index][1] == "relu":
             activation = pre_activation * (pre_activation > 0)
+        # Other activations need to be implemented here as well
+        return activation
+
+    def _backwards_prop(self, w):
+        print("tmp")
 
 
 
 
-abs(np.random.normal(0,1, (4,5)) * (np.random.normal(0,1, (4,5)) > 0))
 
 
 
