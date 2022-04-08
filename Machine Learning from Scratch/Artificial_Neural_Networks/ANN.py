d256575b439c0eff695d0ee4c35048f7e65d3312
isalia20 <irakli.salia854@gmail.com>
2022-04-08 22:13:18 +0400

    fixed some errors, removed some methods

--- Machine Learning from Scratch/Artificial_Neural_Networks/ANN.py
+++ Machine Learning from Scratch/Artificial_Neural_Networks/ANN.py
@@ -6,47 +6,50 @@ class FeedForwardNeuralNetwork:
 
     def __init__(self,
                  random_state=42,
-                 output_threshold = 0.5
+                 output_threshold=0.5,
+                 x=None
                  ):
+        if x is None:
+            raise Exception("You need to initialize the class with X")
         self.random_state = random_state
-        self.network_architecture = {}
         self.weight_matrices = {}
         self.output_threshold = output_threshold
-
-    def _initialize_network(self, x):
-        self.network_architecture[0] = x.shape[1]
+        self.x = x
+        # Initializing network architecture with input shape n_neurons, activation_function(None at layer 0)
+        # and bias neuron(also False) boolean
+        self.network_architecture = {0: (self.x.shape[1], None, False)}
 
     def add_change_layer(self, layer_index, n_neurons, activation_function, bias_neuron = True):
         if layer_index < 0:
-            raise Exception("You can't change/add layer before input")
+            raise Exception("You can't change/add layer before input (negative layer_index)")
         elif layer_index == 0:
             raise Exception("You can't change input layer's neuron count. Change shape of X and reinitialize class")
         self.network_architecture[layer_index] = (n_neurons, activation_function, bias_neuron)
 
-    def _generate_weight_matrices(self, activation_function):
-        for layer_index in range(1, self.network_architecture.keys()):
+    def generate_weight_matrices(self):
+        for layer_index in range(1, len(self.network_architecture.keys())):
             self.weight_matrices[layer_index] = \
-                (np.random.normal(0, 1, (self.network_architecture[layer_index - 1],
-                                        self.network_architecture[layer_index])), # Normal weights
-                 np.random.normal(0, 1, (1, self.network_architecture[layer_index]))) # Bias
+                (np.random.normal(0, 1, (self.network_architecture[layer_index - 1][0],
+                                        self.network_architecture[layer_index][0])),  # Normal weights
+                 np.random.normal(0, 1, (1, self.network_architecture[layer_index][0])))  # Bias
 
-    def _feed_forward(self, previous_layer_output, layer_index):
-        x = np.matrix(previous_layer_output)
+    def feed_forward(self, previous_layer_output, layer_index):
+        x = previous_layer_output
         weight_matrix = self.weight_matrices[layer_index][0]
         bias = self.weight_matrices[layer_index][1]
         if self.network_architecture[layer_index][2]:
-            pre_activation = np.matmul(x, weight_matrix) + bias
+            pre_activation = x @ weight_matrix + bias
         else:
-            pre_activation = np.matmul(x, weight_matrix)
+            pre_activation = x @ weight_matrix
 
         if self.network_architecture[layer_index][1] == "relu":
             activation = pre_activation * (pre_activation > 0)
         elif self.network_architecture[layer_index][1] == "logistic":
-            activation = 1 / (1 + np.exp(pre_activation))
+            activation = 1 / (1 + np.exp(-pre_activation))
 
         return activation
 
-    def _make_prediction(self, last_output):
+    def make_prediction(self, last_output):
         y_pred = last_output >= self.output_threshold
         return y_pred
 
@@ -56,6 +59,4 @@ class FeedForwardNeuralNetwork:
         return loss
 
     def _backwards_prop(self, y, y_pred):
-
-
-
+        return 2
