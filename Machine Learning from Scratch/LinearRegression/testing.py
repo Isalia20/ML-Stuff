92a7ac189a9415abd1604cbc246ed691a7ca7660
isalia20 <irakli.salia854@gmail.com>
2022-04-25 00:33:47 +0400

    added linear regression and respective testing file

--- Machine Learning from Scratch/LinearRegression/testing.py
+++ Machine Learning from Scratch/LinearRegression/testing.py
@@ -1,17 +1,9 @@
 from LinearRegression.Linear_Regression import LinearRegression
 from sklearn.datasets import make_regression
-
-lin_reg = LinearRegression()
-X, y = make_regression(n_samples = 100,n_features= 5)
-
-lin_reg.fit(X, y)
-
-y_pred = lin_reg.predict(X)
-
-
 from sklearn.metrics import mean_squared_error
 
-
+lin_reg = LinearRegression()
+X, y = make_regression(n_samples=100,n_features=5)
+lin_reg.fit(X, y)
+y_pred = lin_reg.predict(X)
 mean_squared_error(y, y_pred)
-
-lin_reg.weight_matrix
